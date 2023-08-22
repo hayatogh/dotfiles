2c74b13bfe11ef05320314c2f97ba64cf9c63306
hayatogh <28914976+hayatogh@users.noreply.github.com>
2023-08-22 18:12:06 +0900

    No commit message

--- cpdots.sh
+++ cpdots.sh
@@ -12,16 +12,16 @@ appdata=$(pspath 'Get-Content Env:APPDATA')
 mkdir -p "$winhome"/.config
 
 if [[ ${1:-} =~ |all ]]; then
-	rsync -a --delete $dotfiles/.vimfx/ "$winhome/.vimfx"
-	rsync -a --delete $dotfiles/.vsvimrc "$winhome/.vsvimrc"
-	rsync -a --delete $dotfiles/PowerShell/ "$windoc/PowerShell"
-	# rsync -a --delete $dotfiles/alacritty/ "$appdata/alacritty"
-	# rsync -a --delete $dotfiles/latexmk/ "$winhome/.config/latexmk"
+	rsync -rt $dotfiles/.vimfx/ "$winhome/.vimfx"
+	rsync -t $dotfiles/.vsvimrc "$winhome/.vsvimrc"
+	rsync -rt $dotfiles/PowerShell/ "$windoc/PowerShell"
+	# rsync -rt $dotfiles/alacritty/ "$appdata/alacritty"
+	# rsync -rt $dotfiles/latexmk/ "$winhome/.config/latexmk"
 fi
 
 if [[ ${1:-} =~ mintty|all ]]; then
-	rsync -a --delete $dotfiles/mintty/ "$appdata/mintty"
+	rsync -rt $dotfiles/mintty/ "$appdata/mintty"
 fi
 if [[ ${1:-} =~ vim|all ]]; then
-	rsync -az --exclude=.git/ --exclude=/.netrwhist --exclude=/.viminfo --exclude=/swap --delete $dotfiles/.vim/ "$winhome/vimfiles"
+	rsync -rtz --exclude=.git/ --exclude=/.netrwhist --exclude=/.viminfo --exclude=/swap --delete $dotfiles/.vim/ "$winhome/vimfiles"
 fi
