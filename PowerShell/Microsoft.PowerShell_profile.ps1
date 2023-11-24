454bfc2a4dd9bba93f40244aef650913317f0398
hayatogh <28914976+hayatogh@users.noreply.github.com>
2023-11-25 01:42:45 +0900

    No commit message

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -71,14 +71,18 @@ function prompt {
 
 Set-Alias upgrade winget_install
 function winget_install {
+  if (!(is_admin)) {
+    Write-Output "Permission denied"
+    return
+  }
   winget install --no-upgrade Discord.Discord MSYS2.MSYS2 Microsoft.PowerToys NurgoSoftware.AquaSnap Valve.Steam
   winget install 7zip.7zip Debian.Debian ImageLine.FLStudio MPC-BE.MPC-BE MichaelTippach.ASIO4ALL Microsoft.MouseandKeyboardCenter Microsoft.WindowsTerminal PeterPawlowski.foobar2000 SteelSeries.GG Streamlink.Streamlink TrackerSoftware.PDF-XChangeEditor Zoom.Zoom
   # BurntSushi.ripgrep.MSVC Git.Git JGraph.Draw sharkdp.fd
 
   _winget_install_ahk
   _winget_install_vim
-  _winget_install_if_outdated "Mintty.WSLtty" '[0-9]\.[0-9]\.[0-9]' "$env:LOCALAPPDATA\wsltty\winget_version"
-  _winget_install_if_outdated "PeterPawlowski.foobar2000.EncoderPack" '20[0-9][0-9]-[0-9][0-9]-[0-9][0-9]' "$env:ProgramFiles\foobar2000\encoders\winget_version"
+  _winget_install_wsltty
+  _winget_install_foobar2000encoderpack
 
   _winget_remove_links
 
@@ -86,6 +90,10 @@ function winget_install {
 }
 
 function _winget_install_ahk {
+  if (!(is_admin)) {
+    Write-Output "Permission denied"
+    return
+  }
   $scripts = (Get-Process -Name AutoHotkey64).CommandLine
   winget install AutoHotkey.AutoHotkey
   if ($?) {
@@ -96,6 +104,10 @@ function _winget_install_ahk {
 }
 
 function _winget_install_vim {
+  if (!(is_admin)) {
+    Write-Output "Permission denied"
+    return
+  }
   $vim = "$env:ProgramFiles\Vim"
   winget install vim.vim
   if ($?) {
@@ -108,12 +120,23 @@ function _winget_install_vim {
   }
 }
 
+function _winget_install_wsltty {
+  _winget_install_if_outdated "Mintty.WSLtty" '[0-9]\.[0-9]\.[0-9]' "$env:LOCALAPPDATA\wsltty\winget_version"
+}
+function _winget_install_foobar2000encoderpack {
+  _winget_install_if_outdated "PeterPawlowski.foobar2000.EncoderPack" '20[0-9][0-9]-[0-9][0-9]-[0-9][0-9]' "$env:ProgramFiles\foobar2000\encoders\winget_version"
+}
+
 function _winget_install_if_outdated {
   param(
     [Parameter(Mandatory)] [string]$package,
     [Parameter(Mandatory)] [string]$ver_regex,
     [Parameter(Mandatory)] [string]$ver_file
   )
+  if (!(is_admin)) {
+    Write-Output "Permission denied"
+    return
+  }
   Write-Output "$package"
   if ("$(winget search --exact $package)" -match $ver_regex) {
     $installed = "0"
@@ -152,6 +175,170 @@ function _winget_remove_links {
     }
   }
   Get-Item "$env:PUBLIC\Desktop\*.lnk" | Remove-Item
+}
+
+function register_shell_all {
+  if (!(is_admin)) {
+    Write-Output "Permission denied"
+    return
+  }
+  _register_shell_wsltty
+  _register_shell_mingw64
+}
+
+function _register_shell_wsltty {
+  if (!(is_admin)) {
+    Write-Output "Permission denied"
+    return
+  }
+  $mintty = $env:LOCALAPPDATA + '\wsltty\bin\mintty.exe'
+  $bg  = '"' + $mintty + '" -i "' + $mintty + '" --WSL=Debian -'
+  $dir = '"' + $mintty + '" -i "' + $mintty + '" --dir "%1" --WSL=Debian -'
+  _register_shell 'wsltty' 'WSL &Shell' $bg $dir $mintty
+}
+
+function _register_shell_mingw64 {
+  if (!(is_admin)) {
+    Write-Output "Permission denied"
+    return
+  }
+  $mingw64 = 'C:\msys64\mingw64.exe'
+  $bg  = '"' + $mingw64 + '" exec /usr/bin/env CHERE_INVOKING=1 /usr/bin/bash -l'
+  $dir = '"' + $mingw64 + '" exec /usr/bin/env CHERE_INVOKING=1 /usr/bin/bash -l'
+  _register_shell 'mingw64' '&MinGW64' $bg $dir $mingw64
+}
+
+function _register_shell {
+  param(
+    [Parameter(Mandatory)] [string]$entry,
+    [Parameter(Mandatory)] [string]$value,
+    [Parameter(Mandatory)] [string]$bg_cmd,
+    [Parameter(Mandatory)] [string]$dir_cmd,
+    [Parameter(Mandatory)] [string]$icon
+  )
+  $reg_bg = 'HKCU:Software\Classes\Directory\Background\shell\' + $entry
+  $reg_bg_cmd = $reg_bg + '\command'
+  New-Item  -Force -Path $reg_bg -Value $value
+  New-ItemProperty -Path $reg_bg -PropertyType String -Name 'Icon' -Value $icon
+  New-Item  -Force -Path $reg_bg_cmd -Value $bg_cmd
+  $reg_dir = 'HKCU:Software\Classes\Directory\shell\' + $entry
+  $reg_dir_cmd = $reg_dir + '\command'
+  New-Item  -Force -Path $reg_dir -Value $value
+  New-ItemProperty -Path $reg_dir -PropertyType String -Name 'Icon' -Value $icon
+  New-Item  -Force -Path $reg_dir_cmd -Value $dir_cmd
+}
+
+function adb_all {
+  ./adb kill-server
+  ./adb devices
+  ./adb shell settings put secure show_rotation_suggestions 0
+}
+
+function setup_windows_sshd {
+  if (!(is_admin)) {
+    Write-Output "Permission denied"
+    return
+  }
+  $programdata = 'C:\ProgramData'
+  if (-not [System.Environment]::GetEnvironmentVariable('PROGRAMDATA', 'Machine')) {
+    [System.Environment]::SetEnvironmentVariable('PROGRAMDATA', $programdata, 'Machine')
+  }
+  if (-Not (Select-String -Quiet "^AuthenticationMethods publickey$" "$programdata\ssh\sshd_config")) {
+    Add-Content "$programdata\ssh\sshd_config" "`r`nAuthenticationMethods publickey"
+  }
+  New-Item "$programdata\ssh\administrators_authorized_keys"
+  Get-Acl "$programdata\ssh\ssh_host_rsa_key" | Set-Acl "$programdata\ssh\administrators_authorized_keys"
+
+  New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShell -Value "C:\Windows\system32\wsl.exe" -PropertyType String -Force
+
+  Set-Service sshd -StartupType Automatic
+  Start-Service sshd
+}
+
+function register_vimmize {
+  if (!(is_admin)) {
+    Write-Output "Permission denied"
+    return
+  }
+  $name     = "vimmizekbd"
+  $action   = New-ScheduledTaskAction -Execute "`"C:\Program Files\AutoHotkey\v2\AutoHotkey64.exe`"" -Argument "`"C:\Users\$env:USERNAME\Box Sync\configs\F13.ahk`""
+  $settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopOnIdleEnd -ExecutionTimeLimit 0 -RestartCount 3 -RestartInterval (New-TimeSpan -Minutes 1)
+  $trigger  = New-ScheduledTaskTrigger -AtLogon -User "$env:COMPUTERNAME\$env:USERNAME"
+
+  Unregister-ScheduledTask -TaskName $name -Confirm:$false -ErrorAction SilentlyContinue
+  Register-ScheduledTask -TaskName $name -Action $action -Settings $settings -Trigger $trigger -RunLevel Highest
+}
+
+function fontlink {
+  if (!(is_admin)) {
+    Write-Output "Permission denied"
+    return
+  }
+  New-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\FontLink\SystemLink\" -Name Consolas -Value 'HGRGM.TTC,HGRGM' -PropertyType MultiString -Force
+}
+
+function create_shortcuts {
+  $document = [Environment]::GetFolderPath("MyDocuments")
+  $box = $env:USERPROFILE + "\Box Sync"
+
+  $shortcuts = @(
+    @{
+      name   = $document + "\System StartMenu.lnk";
+      target = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs";
+    };
+    @{
+      name   = $document + "\StartMenu.lnk";
+      target = $env:APPDATA + "\Microsoft\Windows\Start Menu\Programs";
+    };
+    @{
+      name   = $document + "\Startup.lnk";
+      target = $env:APPDATA + "\Microsoft\Windows\Start Menu\Programs\Startup";
+    };
+    @{
+      name   = $document + "\TaskBar.lnk";
+      target = $env:APPDATA + "\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar";
+    };
+    @{
+      name   = $document + "\firefox - 2.lnk";
+      target = "C:\Program Files\Firefox Developer Edition\firefox.exe";
+      args   = "-P X";
+    };
+    @{
+      name   = $document + "\F13.ahk.lnk";
+      target = $box + "\configs\F13.ahk";
+    };
+    @{
+      name   = $document + "\ToggleSpeaker.lnk";
+      target = "C:\WINDOWS\System32\WindowsPowerShell\v1.0\powershell.exe";
+      args   = "-ExecutionPolicy Bypass -NoProfile -File `"" + $box + "\configs\ToggleSpeaker.ps1`"";
+      style  = 7;
+    };
+    @{
+      name   = $document + "\WSL.lnk";
+      target = $env:LOCALAPPDATA + "\wsltty\bin\mintty.exe"
+      args   = "--WSL -~ -";
+    };
+  )
+
+  $shell = New-Object -ComObject "WScript.Shell"
+  $shortcuts | % {
+    $s = $shell.CreateShortcut($_.name)
+    $s.TargetPath = $_.target
+    if( $_.ContainsKey('args') ) {
+      $s.Arguments = $_.args
+    }
+    if( $_.ContainsKey('wd') ) {
+      $s.WorkingDirectory = $_.wd
+    }
+    if( $_.ContainsKey('style') ) {
+      $s.WindowStyle = $_.style
+    }
+    $s.Save()
+  }
+}
+
+function is_admin {
+  (New-Object Security.Principal.WindowsPrincipal ([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
 }
 
 Invoke-Command -ScriptBlock {
