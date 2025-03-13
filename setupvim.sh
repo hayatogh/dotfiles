bea32eeadd18053872a098075d5c9153406aea1b
hayatogh <28914976+hayatogh@users.noreply.github.com>
2025-03-14 00:22:40 +0900

    No commit message

--- setupvim.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-rm -rf ~/.vim/pack/minpac/opt/minpac
-git clone --depth 1 -- https://github.com/hayatogh/minpac.git ~/.vim/pack/minpac/opt/minpac &>/dev/null
-vim -ENs -u ~/.vim/vimrc -i NONE '+MinLoad' '+q'
